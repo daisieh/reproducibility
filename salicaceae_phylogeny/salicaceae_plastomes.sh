4298aa902e9cfc3d4872d34630159186c11af57b
daisieh_local <daisieh@gmail.com>
2014-07-11 16:56:08 -0700

    should only run on good files

--- salicaceae_phylogeny/salicaceae_plastomes.sh
+++ salicaceae_phylogeny/salicaceae_plastomes.sh
@@ -16,29 +16,32 @@ OUTNAME=$RESULTDIR/$INFILE
 
 mkdir $RESULTDIR
 
-
-# run the bam to vcf pipeline:
-
-# OUTNAME.1.txt has sample,server,path
-gawk -F "\t" 'NR > 1 {print $2,$4,$5}' $INFILE > $OUTNAME.1.txt
-
 # check to remove any files to paths that don't exist on this machine.
 while read line
 do
 arr=($line);
-if [ -f ${arr[2]} ];
+if [ -f ${arr[4]} ];
 then
-echo ${arr[2]} >> $OUTNAME.2.txt;
+echo $line >> $OUTNAME.0.txt;
 fi
-done < $OUTNAME.1.txt
+done < $INFILE
 
-# cd $RESULTDIR
-# bash $REPOS/phylogenomics/pipelines/bam_to_plastome_vcf.sh $OUTNAME.1.txt Manihot_cp.fasta
-# cd ..
-#
-# # convert the vcfs to fasta:
-# gawk -F "\t" 'NR > 1 {print $1".vcf"}' $INFILE > $OUTNAME.2.txt
-# perl $REPOS/phylogenomics/converting/vcf2fasta.pl -samples $OUTNAME.2.txt -output $OUTNAME -thresh 0 -cov 300
-#
-# # trim missing data at the 0.1 missing threshold:
-# perl $REPOS/phylogenomics/parsing/trim_missing.pl -in $OUTNAME.fasta -out $OUTNAME.trimmed.fasta -row 1.0 -col 0.1
+INFILE=$OUTNAME.0.txt;
+echo $INFILE;
+
+# run the bam to vcf pipeline:
+
+OUTNAME.1.txt has sample,server,path
+gawk -F "\t" 'NR > 1 {print $2,$4,$5}' $INFILE > $OUTNAME.1.txt
+
+
+cd $RESULTDIR
+bash $REPOS/phylogenomics/pipelines/bam_to_plastome_vcf.sh $OUTNAME.2.txt Manihot_cp.fasta
+cd ..
+
+convert the vcfs to fasta:
+gawk -F "\t" 'NR > 1 {print $1".vcf"}' $INFILE > $OUTNAME.2.txt
+perl $REPOS/phylogenomics/converting/vcf2fasta.pl -samples $OUTNAME.2.txt -output $OUTNAME -thresh 0 -cov 300
+
+trim missing data at the 0.1 missing threshold:
+perl $REPOS/phylogenomics/parsing/trim_missing.pl -in $OUTNAME.fasta -out $OUTNAME.trimmed.fasta -row 1.0 -col 0.1

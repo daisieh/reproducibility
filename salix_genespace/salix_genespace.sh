577ae97c8d4035c4557dc50241c9c946dbe6f2f8
daisieh_local <daisieh@gmail.com>
2014-07-15 17:09:13 -0700

    adding more beginning steps

--- salix_genespace/salix_genespace.sh
+++ salix_genespace/salix_genespace.sh
@@ -7,11 +7,38 @@
 #PBS -j oe
 #PBS -l epilogue=/home/daisieh/epilogue.script
 
-refdir=/home/daisieh/refs/pop_refs
+OUTDIR=$1
+REFDIR=$OUTDIR/pop_refs
+
+# The Populus trichocarpa reference annotation is available at: http://genome.jgi.doe.gov/pages/dynamicOrganismDownload.jsf?organism=Ptrichocarpa#
+POTRI_GFF=Ptrichocarpa_210_gene_exons.gff3
+
+# The Populus trichocarpa reference genome is available at: http://genome.jgi.doe.gov/pages/dynamicOrganismDownload.jsf?organism=Ptrichocarpa#
+POTRI_FASTA=Ptrichocarpa_210.fasta
+salix_bam=/data/raid50/GalaxyData/Populus/PopulusTrichocarpa/DNA/2012-08-17/IX0393/C10ERACXX_5/IX0393_C10ERACXX_5_ACAAAC.bam
+SALIX="Sal_erio"
+
+samtools view $salix_bam | gawk '{if (and($2,0x0200)) next; if (and($2,0x0040)) {print ">"$1"/1\n"$10; next;} if (and($2,0x0080)) {print ">"$1"/2\n"$10; next;} print ">"$1"\n"$10;}' - > $SALIX.genome.fasta
 
 # prepare the aTRAM database
+perl $REPOS/aTRAM/format_sra.pl -input $SALIX.genome.fasta -output $OUTDIR/$SALIX.db
+rm $SALIX.genome.fasta
 
-# split genes into batches so that we can pipeline more batches at once:
+printf "$SALIX\t$OUTDIR/$SALIX.db.atram\n" > $OUTDIR/$SALIX.txt
+
+grep "gene" $POTRI_GFF | gawk -F "\t" '{print $9;}' | head -n 5 | gawk -F ";Name=" '{print $2;}' > $OUTDIR/ref_genes.txt
+
+# slice files for reference
+perl $REPOS/phylogenomics/parsing/slice_gff_from_fasta.pl -gff $POTRI_GFF -fasta $POTRI_FASTA -out $REFDIR -gene $OUTDIR/ref_genes.txt
+
+# now we have a directory, $REFDIR, filled with fasta references for all possible genes. We actually only want to work on the first isoform's first CDS, so find just those:
+
+mkdir $REFDIR/cds
+while read g
+do
+	perl $REPOS/phylogenomics/filtering/select_one_from_fasta.pl -fasta $REFDIR/$g.fasta -outputfile $REFDIR/cds/$g -seq $g.1.CDS.1
+
+done < $OUTDIR/ref_genes.txt
 
 # run aTRAM BasicPipeline on all genes
 perl $REPOS/aTRAM/Pipelines/BasicPipeline.pl -samples $OUTDIR/$SALIX.txt -targets /home/daisieh/cds_fragments/cds00 -out /home/daisieh/cds00 -processes 8 -iter 5 -max_memory 8 -debug

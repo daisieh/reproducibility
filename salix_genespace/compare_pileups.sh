e47ff375842cdc728dd55eefa9831b754762a84d
daisieh_local <daisieh@gmail.com>
2014-07-21 11:45:59 -0700

    remove unmapped seqs

--- salix_genespace/compare_pileups.sh
+++ salix_genespace/compare_pileups.sh
@@ -8,11 +8,13 @@ FASTA=$2
 bwa index $FASTA
 
 python $REPOS/phylogenomics/python/bwa_to_bam.py -i $SAMPLE -r $FASTA -p 8 -n 10000000
+# remove unmapped pairs
+samtools view -F 4 -b $samplename.bam > $samplename.reduced.bam
 
 # sort the bam files
 while read line
 do
 arr=($line);
 samplename=${arr[1]};
-samtools sort $samplename.bam $samplename.sorted
+samtools sort $samplename.reduced.bam $samplename.sorted
 done < $SAMPLE

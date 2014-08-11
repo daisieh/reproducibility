a834f72cd59e265f5a6a81f1d4a3c1a279af681a
daisieh_local <daisieh@gmail.com>
2014-08-11 14:36:08 -0700

    everything up to trimming

--- plastome_phylogeography/balsam_analysis.sh
+++ plastome_phylogeography/balsam_analysis.sh
@@ -25,35 +25,35 @@ then
 echo $line >> $OUTNAME.0.txt;
 fi
 done < $INFILE
-
+#
 INFILE=$OUTNAME.0.txt;
 
 # run the bam to vcf pipeline:
 
-# OUTNAME.1.txt has server,sample,path
+OUTNAME.1.txt has server,sample,path
 gawk -F " " '{print $12"\t"$1"\t"$13}' $INFILE > $OUTNAME.1.txt
-
+#
 cd $RESULTDIR
 bash $REPOS/phylogenomics/pipelines/bam_to_plastome_vcf.sh ../$OUTNAME.1.txt
 cd ..
-
+#
 # convert the vcfs to fasta:
 gawk -F "\t" 'NR > 1 {print $1".vcf"}' $INFILE > $OUTNAME.2.txt
 perl $REPOS/phylogenomics/converting/vcf2fasta.pl -samples $OUTNAME.2.txt -output $OUTNAME -thresh 0 -cov 300
-
+#
 # trim missing data at the 0.1 missing threshold:
-perl $REPOS/phylogenomics/parsing/trim_missing.pl -in $OUTNAME.fasta -out $OUTNAME.trimmed.fasta -row 0.1 -col 0.1
+# perl $REPOS/phylogenomics/parsing/trim_missing.pl -in $OUTNAME.fasta -out $OUTNAME.trimmed.fasta -row 0.1 -col 0.1
 
 # perform downstream analyses:
 
 # generate a KML file for the populations:
-gawk -F "\t" 'NR > 1 {print $5,$6,$11,$10}' $INFILE | sort | uniq > $OUTNAME.locs.txt
-perl $REPOS/phylogenomics/reporting/make_kml.pl -i $OUTNAME.locs.txt -c species_colors.txt -o $OUTNAME
-
-#rename samples by locality:
-#create a locality mapping:
-gawk -F "\t" 'NR > 1 {print $1,$2,$10}' $INFILE | gawk '{if ($3 < -110) {print $1,"W_"$2;next;}; if ($3 < -79) {print $1,"C_"$2;next;}; if ($3 < -50) {print $1,"E_"$2;next;}}' > $OUTNAME.rename.locs
-perl $REPOS/phylogenomics/converting/relabel_samples.pl -i $OUTNAME.trimmed.fasta -label $OUTNAME.rename.locs -out $OUTNAME.locs.fasta
-
-#convert to nexus:
-perl $REPOS/phylogenomics/converting/convert_file.pl $OUTNAME.locs.fasta $OUTNAME.locs.nex
+# gawk -F "\t" 'NR > 1 {print $5,$6,$11,$10}' $INFILE | sort | uniq > $OUTNAME.locs.txt
+# perl $REPOS/phylogenomics/reporting/make_kml.pl -i $OUTNAME.locs.txt -c species_colors.txt -o $OUTNAME
+#
+# #rename samples by locality:
+# #create a locality mapping:
+# gawk -F "\t" 'NR > 1 {print $1,$2,$10}' $INFILE | gawk '{if ($3 < -110) {print $1,"W_"$2;next;}; if ($3 < -79) {print $1,"C_"$2;next;}; if ($3 < -50) {print $1,"E_"$2;next;}}' > $OUTNAME.rename.locs
+# perl $REPOS/phylogenomics/converting/relabel_samples.pl -i $OUTNAME.trimmed.fasta -label $OUTNAME.rename.locs -out $OUTNAME.locs.fasta
+#
+# #convert to nexus:
+# perl $REPOS/phylogenomics/converting/convert_file.pl $OUTNAME.locs.fasta $OUTNAME.locs.nex

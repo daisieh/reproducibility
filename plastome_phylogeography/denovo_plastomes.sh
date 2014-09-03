3414020633dcfaf17f8c125a375b0d5e2a597ed7
daisieh_local <daisieh@gmail.com>
2014-09-03 16:06:59 -0700

    finish atram gap-filling

--- plastome_phylogeography/denovo_plastomes.sh
+++ plastome_phylogeography/denovo_plastomes.sh
@@ -29,6 +29,7 @@ echo "processing $sample..."
 
 #### aTRAM libs
 # perl $REPOS/aTRAM/format_sra.pl -in $sample.fasta -out aTRAMdbs/$sample -num 10
+# echo "$sample\t$aTRAMdbs/$sample.atram\n" >> atram_samples.txt
 
 #### use CLC genomics workbench 7.0.3 to do de novo assembly of the reads
 #### rename the outputted contigs to more sensible names.
@@ -37,11 +38,53 @@ echo "processing $sample..."
 #### use velvet
 # velveth $sample 31 -shortPaired -fasta -interleaved $sample.fasta
 # velvetg $sample -cov_cutoff 20 -ins_length 400 -min_contig_lgth 300
-# sed s/cov_\d+\.\d+// < $sample/contigs.fa > $sample.contigs.fasta
+
+#### rename contigs from NODE_2_length_25848_cov_191.293564 to $sample_
+# sed s/NODE/$sample/ < $sample/contigs.fa | sed s/_length.*// > $sample.contigs.fasta
+
 #### make draft plastome
 # perl $REPOS/phylogenomics/plastome/contigs_to_cp.pl -ref $reffile -contig $sample.contigs.fasta -out $sample.plastome
 
 #### clean draft plastome
 # perl $REPOS/phylogenomics/plastome/clean_cp.pl -ref $reffile -contig $sample.plastome.draft.fasta -out $sample.plastome
+done < $samplefile
+
+echo "Finished initial assembly"
+#### further cleanup steps
+
+while read line
+do
+arr=($line);
+sample=${arr[1]}
+echo "filling in $sample..."
+
+#### find sections of ambiguity in the cleaned plastome:
+grep -o -E ".{100}N+.{100}" $sample.plastome.cleaned.fasta > $sample.to_atram.txt
+
+count=1
+while read seq
+do
+echo ">$sample.$count\n$seq\n" > $sample.$count.fasta
+echo "$sample.$count\t$sample.$count.fasta\n" >> $sample.targets.txt
+count=count+1
+done < $sample.to_atram.txt
+echo "$sample\t$aTRAMdbs/$sample.atram" > $sample.samples.txt
+
+#### aTRAM those ambiguous sections
+perl $REPOS/aTRAM/Pipelines/BasicPipeline.pl -samples $sample.samples.txt -target $sample.targets.txt -frac 0.3 -iter 5 -out $sample.atram
+
+#### Now, take the best seq from each one and align it to the draft:
+cat $sample.plastome.cleaned.fasta > $sample.plastome.toaln.fasta
+for reg in {1..$count}
+do
+head -n 2 $sample.atram/$sample.$reg.best.fasta >> $sample.plastome.toaln.fasta
+done
+
+#### align these with mafft
+
+mafft --auto $sample.plastome.toaln.fasta > $sample.plastome.aln.fasta
+
+#### consolidate into consensus sequence
+perl $REPOS/phylogenomics/filtering/consensus.pl $sample.plastome.aln.fasta > $sample.plastome.final.fasta
 
 done < $samplefile

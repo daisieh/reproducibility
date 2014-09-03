6cfbba357a372d66bb1366567b5eef54523e6231
daisieh_local <daisieh@gmail.com>
2014-09-03 16:08:52 -0700

    try running whole thing

--- plastome_phylogeography/denovo_plastomes.sh
+++ plastome_phylogeography/denovo_plastomes.sh
@@ -16,7 +16,7 @@ done < $1
 samplefile="samplefile.txt"
 
 #### Take raw files and subset part for analysis:
-# python $REPOS/phylogenomics/python/subset_bam.py -i $samplefile -n 6 -p 4
+python $REPOS/phylogenomics/python/subset_bam.py -i $samplefile -n 6 -p 4
 
 #### $samplefile has a sample file with server, name, path
 while read line
@@ -25,28 +25,28 @@ arr=($line);
 sample=${arr[1]}
 echo "processing $sample..."
 
-# $REPOS/phylogenomics/converting/bam_to_fasta.sh $sample.small.bam $sample
+$REPOS/phylogenomics/converting/bam_to_fasta.sh $sample.small.bam $sample
 
 #### aTRAM libs
-# perl $REPOS/aTRAM/format_sra.pl -in $sample.fasta -out aTRAMdbs/$sample -num 10
-# echo "$sample\t$aTRAMdbs/$sample.atram\n" >> atram_samples.txt
+perl $REPOS/aTRAM/format_sra.pl -in $sample.fasta -out aTRAMdbs/$sample -num 10
+echo "$sample\t$aTRAMdbs/$sample.atram\n" >> atram_samples.txt
 
 #### use CLC genomics workbench 7.0.3 to do de novo assembly of the reads
 #### rename the outputted contigs to more sensible names.
 # sed s/\.small\.bam_.no_read_group._.paired.// < $sample.contigs.fa | sed s/Average\ coverage:.*$// > $sample.contigs.fasta
 
 #### use velvet
-# velveth $sample 31 -shortPaired -fasta -interleaved $sample.fasta
-# velvetg $sample -cov_cutoff 20 -ins_length 400 -min_contig_lgth 300
+velveth $sample 31 -shortPaired -fasta -interleaved $sample.fasta
+velvetg $sample -cov_cutoff 20 -ins_length 400 -min_contig_lgth 300
 
 #### rename contigs from NODE_2_length_25848_cov_191.293564 to $sample_
-# sed s/NODE/$sample/ < $sample/contigs.fa | sed s/_length.*// > $sample.contigs.fasta
+sed s/NODE/$sample/ < $sample/contigs.fa | sed s/_length.*// > $sample.contigs.fasta
 
 #### make draft plastome
-# perl $REPOS/phylogenomics/plastome/contigs_to_cp.pl -ref $reffile -contig $sample.contigs.fasta -out $sample.plastome
+perl $REPOS/phylogenomics/plastome/contigs_to_cp.pl -ref $reffile -contig $sample.contigs.fasta -out $sample.plastome
 
 #### clean draft plastome
-# perl $REPOS/phylogenomics/plastome/clean_cp.pl -ref $reffile -contig $sample.plastome.draft.fasta -out $sample.plastome
+perl $REPOS/phylogenomics/plastome/clean_cp.pl -ref $reffile -contig $sample.plastome.draft.fasta -out $sample.plastome
 done < $samplefile
 
 echo "Finished initial assembly"

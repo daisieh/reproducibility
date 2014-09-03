a3577b71049611780b08500c129df1270c7055b4
daisieh_local <daisieh@gmail.com>
2014-09-03 16:38:08 -0700

    temporary commenting

--- plastome_phylogeography/denovo_plastomes.sh
+++ plastome_phylogeography/denovo_plastomes.sh
@@ -24,43 +24,43 @@ while read line
 do
 arr=($line);
 sample=${arr[1]}
-echo "processing $sample..."
-echo "  making fasta from bam"
-$REPOS/phylogenomics/converting/bam_to_fasta.sh $sample.small.bam $sample
-
-#### aTRAM libs
-echo "  making aTRAM db"
-perl $REPOS/aTRAM/format_sra.pl -in $sample.fasta -out aTRAMdbs/$sample -num 10
-echo "$sample\t$aTRAMdbs/$sample.atram\n" >> atram_samples.txt
-
-#### use CLC genomics workbench 7.0.3 to do de novo assembly of the reads
-#### rename the outputted contigs to more sensible names.
-# sed s/\.small\.bam_.no_read_group._.paired.// < $sample.contigs.fa | sed s/Average\ coverage:.*$// > $sample.contigs.fasta
-
-#### use velvet
-echo "  assembling contigs with Velvet"
-velveth $sample 31 -shortPaired -fasta -interleaved $sample.fasta
-velvetg $sample -cov_cutoff 20 -ins_length 400 -min_contig_lgth 300
-
-#### rename contigs from NODE_2_length_25848_cov_191.293564 to $sample_
-sed s/NODE/$sample/ < $sample/contigs.fa | sed s/_length.*// > $sample.contigs.fasta
-
-#### make draft plastome
-echo "  assembling draft plastome from contigs"
-perl $REPOS/phylogenomics/plastome/contigs_to_cp.pl -ref $reffile -contig $sample.contigs.fasta -out $sample.plastome
-
-#### clean draft plastome
-perl $REPOS/phylogenomics/plastome/clean_cp.pl -ref $reffile -contig $sample.plastome.draft.fasta -out $sample.plastome
-done < $samplefile
-
-echo "Finished initial assembly"
-#### further cleanup steps
-
-while read line
-do
-arr=($line);
-sample=${arr[1]}
-echo "filling in $sample..."
+# echo "processing $sample..."
+# echo "  making fasta from bam"
+# $REPOS/phylogenomics/converting/bam_to_fasta.sh $sample.small.bam $sample
+#
+# #### aTRAM libs
+# echo "  making aTRAM db"
+# perl $REPOS/aTRAM/format_sra.pl -in $sample.fasta -out aTRAMdbs/$sample -num 10
+# echo "$sample\t$aTRAMdbs/$sample.atram\n" >> atram_samples.txt
+#
+# #### use CLC genomics workbench 7.0.3 to do de novo assembly of the reads
+# #### rename the outputted contigs to more sensible names.
+# # sed s/\.small\.bam_.no_read_group._.paired.// < $sample.contigs.fa | sed s/Average\ coverage:.*$// > $sample.contigs.fasta
+#
+# #### use velvet
+# echo "  assembling contigs with Velvet"
+# velveth $sample 31 -shortPaired -fasta -interleaved $sample.fasta
+# velvetg $sample -cov_cutoff 20 -ins_length 400 -min_contig_lgth 300
+#
+# #### rename contigs from NODE_2_length_25848_cov_191.293564 to $sample_
+# sed s/NODE/$sample/ < $sample/contigs.fa | sed s/_length.*// > $sample.contigs.fasta
+#
+# #### make draft plastome
+# echo "  assembling draft plastome from contigs"
+# perl $REPOS/phylogenomics/plastome/contigs_to_cp.pl -ref $reffile -contig $sample.contigs.fasta -out $sample.plastome
+#
+# #### clean draft plastome
+# perl $REPOS/phylogenomics/plastome/clean_cp.pl -ref $reffile -contig $sample.plastome.draft.fasta -out $sample.plastome
+# done < $samplefile
+#
+# echo "Finished initial assembly"
+# #### further cleanup steps
+#
+# while read line
+# do
+# arr=($line);
+# sample=${arr[1]}
+# echo "filling in $sample..."
 
 #### find sections of ambiguity in the cleaned plastome:
 grep -o -E ".{100}[Nn]+.{100}" $sample.plastome.cleaned.fasta > $sample.to_atram.txt

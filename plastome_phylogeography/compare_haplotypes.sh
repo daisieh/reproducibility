61b33d74a767cff14ba77c4bd4c53822e7c29b45
daisieh_local <daisieh@gmail.com>
2014-09-13 23:52:32 -0700

    samplefile

--- plastome_phylogeography/compare_haplotypes.sh
+++ plastome_phylogeography/compare_haplotypes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #### check to remove any files to paths that don't exist on this machine.
-samplefile=$2/samplefile.txt
+samplefile=samplefile.txt
 
 # rm $samplefile
 # gawk '$0 !~ /^#/' $1 |
@@ -17,16 +17,19 @@ samplefile=$2/samplefile.txt
 # done;
 # };
 
+gawk -F " " '{print $11"\t"$1"\t"$12}' $1 > $samplefile
+
+
 for ref in $2/*
 do
 	filename=$(basename "$ref")
 	refname="${filename%.*}"
 
 	mkdir $refname
-	cd $refname
 	#### $samplefile has a sample file with server, name, path
 	while read line
 	do
+		cd $refname
 		arr=($line);
 		sample=${arr[1]}
 		echo "processing $sample..."
@@ -37,6 +40,6 @@ do
 		samtools mpileup -B -C50 -I -f $ref -u $sample.sorted.bam > $sample.bcf
 		bcftools view -c $sample.bcf > $sample.vcf
 		rm $sample.bcf
+		cd ..
 	done < $1
-	cd ..
 done

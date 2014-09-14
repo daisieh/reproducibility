6aad297d04656915625cd52dc69f88fbc2245aad
daisieh_local <daisieh@gmail.com>
2014-09-13 23:48:50 -0700

    test

--- plastome_phylogeography/compare_haplotypes.sh
+++ plastome_phylogeography/compare_haplotypes.sh
@@ -3,19 +3,19 @@
 #### check to remove any files to paths that don't exist on this machine.
 samplefile=$2/samplefile.txt
 
-rm $samplefile
-gawk '$0 !~ /^#/' $1 |
-{
-while read line
-do
-arr=($line);
-if [ -f ${arr[2]} ];
-then
-echo $line
-echo $line >> $samplefile;
-fi
-done;
-};
+# rm $samplefile
+# gawk '$0 !~ /^#/' $1 |
+# {
+# while read line
+# do
+# arr=($line);
+# if [ -f ${arr[2]} ];
+# then
+# echo $line
+# echo $line >> $samplefile;
+# fi
+# done;
+# };
 
 for ref in $2/*
 do
@@ -27,16 +27,16 @@ do
 	#### $samplefile has a sample file with server, name, path
 	while read line
 	do
-	arr=($line);
-	sample=${arr[1]}
-	echo "processing $sample..."
-	echo "using $ref as reference"
+		arr=($line);
+		sample=${arr[1]}
+		echo "processing $sample..."
+		echo "using $ref as reference"
 
-	python $REPOS/phylogenomics/python/bowtie_align.py -i $sample.txt -r $ref -p 8 -n 2000000
+		python $REPOS/phylogenomics/python/bowtie_align.py -i $sample.txt -r $ref -p 8 -n 2000000
 
-	samtools mpileup -B -C50 -I -f $ref -u $sample.sorted.bam > $sample.bcf
-	bcftools view -c $sample.bcf > $sample.vcf
-	rm $sample.bcf
-	done < $samplefile
+		samtools mpileup -B -C50 -I -f $ref -u $sample.sorted.bam > $sample.bcf
+		bcftools view -c $sample.bcf > $sample.vcf
+		rm $sample.bcf
+	done < $1
 	cd ..
 done

cd4063712631cce327cfae41dcf3458790a744c9
daisieh_local <daisieh@gmail.com>
2014-09-13 23:34:37 -0700

    compare bam subsets to references

---
+++ plastome_phylogeography/compare_haplotypes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#### check to remove any files to paths that don't exist on this machine.
+samplefile="samplefile.txt"
+
+for ref in $2
+	rm $samplefile
+	gawk '$0 !~ /^#/' $1 |
+	{
+	while read line
+	do
+	arr=($line);
+	if [ -f ${arr[2]} ];
+	then
+	echo $line >> $samplefile;
+	fi
+	done;
+	};
+
+	#### $samplefile has a sample file with server, name, path
+	while read line
+	do
+	arr=($line);
+	sample=${arr[1]}
+	echo "processing $sample..."
+	echo "using $ref as reference"
+
+	python $REPOS/phylogenomics/python/bowtie_align.py -i $sample.txt -r $ref -p 8 -n 2000000
+
+	samtools mpileup -B -C50 -I -f $ref -u $sample.sorted.bam > $sample.bcf
+	bcftools view -c $sample.bcf > $sample.vcf
+	rm $sample.bcf
+
+	done < $samplefile
+done

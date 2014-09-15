c2c059b3c5e05b9fe333032bb2757ac7b26d5d20
daisieh_local <daisieh@gmail.com>
2014-09-15 10:33:06 -0400

    check commands

--- plastome_phylogeography/compare_haplotypes.sh
+++ plastome_phylogeography/compare_haplotypes.sh
@@ -45,11 +45,13 @@ do
 				echo "processing $sample..."
 
 # 				python $REPOS/phylogenomics/python/bowtie_align.py -i ../$samplefile -r $ref -p 8 -n 2000000
+    			echo "samtools view $location | head -n 2000000 | samtools view -S -u > $sample.small.bam"
     			samtools view $location | head -n 2000000 | samtools view -S -u > $sample.small.bam
 #
         		$REPOS/phylogenomics/converting/bam_to_fastq.sh $sample.small.bam $sample
         		$REPOS/phylogenomics/converting/unpair_seqs.pl $sample.fastq $sample
         		bowtie2 -p 8 --no-unal --no-discordant --no-mixed --no-contain --no-unal -x ../$refname.index -1 $sample.1.fastq -2 $sample.2.fastq -S $sample.sam
+				echo "samtools view -S -b -u $sample.sam | samtools view -F 4 -b - > $sample.reduced.bam"
 		        samtools view -S -b -u $sample.sam | samtools view -F 4 -b - > $sample.reduced.bam
 		        rm $sample.sam
 				mv $sample.reduced.bam $sample.bam

3526d2ae6f313673c3315e64c2f4c5d211d13851
daisieh_local <daisieh@gmail.com>
2014-09-15 10:22:05 -0400

    linearize

--- plastome_phylogeography/compare_haplotypes.sh
+++ plastome_phylogeography/compare_haplotypes.sh
@@ -25,6 +25,7 @@ do
 	filename=$(basename "$ref")
 	refname="${filename%.*}"
 
+	bowtie2-build $refname $refname.index
 	mkdir $refname
 	#### $samplefile has a sample file with server, name, path
 	while read line
@@ -33,6 +34,7 @@ do
 		echo "using $ref as reference"
 		arr=($line);
 		sample=${arr[1]}
+		location=${arr[2]}
 		echo ${arr[2]}
 		if [ -f $sample.vcf ]
 		then
@@ -42,8 +44,16 @@ do
 			then
 				echo "processing $sample..."
 
-				python $REPOS/phylogenomics/python/bowtie_align.py -i ../$samplefile -r $ref -p 8 -n 2000000
+    			samtools view $location | head -n 2000000 | samtools view -S -u > $sample.small.bam
 
+        		$REPOS/phylogenomics/converting/bam_to_fastq.sh $sample.small.bam $sample
+        		$REPOS/phylogenomics/converting/unpair_seqs.pl $sample.fastq $sample
+        		bowtie2 -p 8 --no-unal --no-discordant --no-mixed --no-contain --no-unal -x ../$refname.index -1 $sample.1.fastq -2 $sample.2.fastq -S $sample.sam
+		        samtools view -S -b -u $sample.sam | samtools view -F 4 -b - > $sample.reduced.bam
+		        rm $sample.sam
+				mv $sample.reduced.bam $sample.bam
+				samtools sort $sample.bam $sample.sorted
+				rm $sample.bam $sample.fastq $sample.*.fastq
 				samtools mpileup -B -C50 -I -f $ref -u $sample.sorted.bam > $sample.bcf
 				bcftools view -c $sample.bcf > $sample.vcf
 				rm $sample.bcf $sample.sorted.bam $sample.small.bam

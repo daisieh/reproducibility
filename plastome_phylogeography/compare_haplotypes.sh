53280e515e0409940685dc72531c66b114d3abf4
daisieh_local <daisieh@gmail.com>
2014-09-15 11:11:19 -0400

    don't remove twice

--- plastome_phylogeography/compare_haplotypes.sh
+++ plastome_phylogeography/compare_haplotypes.sh
@@ -44,10 +44,9 @@ do
 				echo "using $ref as reference"
 				echo "processing $sample..."
 
-# 				python $REPOS/phylogenomics/python/bowtie_align.py -i ../$samplefile -r $ref -p 8 -n 2000000
     			echo "samtools view $location | head -n 2000000 | samtools view -S -u - > $sample.small.bam"
     			samtools view $location | head -n 2000000 | samtools view -S -u - > $sample.small.bam
-#
+
         		$REPOS/phylogenomics/converting/bam_to_fastq.sh $sample.small.bam $sample
         		$REPOS/phylogenomics/converting/unpair_seqs.pl $sample.fastq $sample
         		bowtie2 -p 8 --no-unal --no-discordant --no-mixed --no-contain --no-unal -x ../$refname.index -1 $sample.1.fastq -2 $sample.2.fastq -S $sample.sam
@@ -59,7 +58,7 @@ do
 				rm $sample.bam $sample.fastq $sample.*.fastq
 				samtools mpileup -B -C50 -I -f $ref -u $sample.sorted.bam > $sample.bcf
 				bcftools view -c $sample.bcf > $sample.vcf
-				rm $sample.bcf $sample.sorted.bam $sample.small.bam
+				rm $sample.bcf $sample.sorted.bam
 			fi
 		fi
 		cd ..

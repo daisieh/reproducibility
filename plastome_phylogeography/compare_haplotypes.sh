dd160ee80f793ab33c074142b6e97188b06dc3ad
daisieh_local <daisieh@gmail.com>
2014-09-15 10:23:59 -0400

    temp comment out

--- plastome_phylogeography/compare_haplotypes.sh
+++ plastome_phylogeography/compare_haplotypes.sh
@@ -44,15 +44,16 @@ do
 			then
 				echo "processing $sample..."
 
-    			samtools view $location | head -n 2000000 | samtools view -S -u > $sample.small.bam
-
-        		$REPOS/phylogenomics/converting/bam_to_fastq.sh $sample.small.bam $sample
-        		$REPOS/phylogenomics/converting/unpair_seqs.pl $sample.fastq $sample
-        		bowtie2 -p 8 --no-unal --no-discordant --no-mixed --no-contain --no-unal -x ../$refname.index -1 $sample.1.fastq -2 $sample.2.fastq -S $sample.sam
-		        samtools view -S -b -u $sample.sam | samtools view -F 4 -b - > $sample.reduced.bam
-		        rm $sample.sam
-				mv $sample.reduced.bam $sample.bam
-				samtools sort $sample.bam $sample.sorted
+# 				python $REPOS/phylogenomics/python/bowtie_align.py -i ../$samplefile -r $ref -p 8 -n 2000000
+#     			samtools view $location | head -n 2000000 | samtools view -S -u > $sample.small.bam
+#
+#         		$REPOS/phylogenomics/converting/bam_to_fastq.sh $sample.small.bam $sample
+#         		$REPOS/phylogenomics/converting/unpair_seqs.pl $sample.fastq $sample
+#         		bowtie2 -p 8 --no-unal --no-discordant --no-mixed --no-contain --no-unal -x ../$refname.index -1 $sample.1.fastq -2 $sample.2.fastq -S $sample.sam
+# 		        samtools view -S -b -u $sample.sam | samtools view -F 4 -b - > $sample.reduced.bam
+# 		        rm $sample.sam
+# 				mv $sample.reduced.bam $sample.bam
+# 				samtools sort $sample.bam $sample.sorted
 				rm $sample.bam $sample.fastq $sample.*.fastq
 				samtools mpileup -B -C50 -I -f $ref -u $sample.sorted.bam > $sample.bcf
 				bcftools view -c $sample.bcf > $sample.vcf

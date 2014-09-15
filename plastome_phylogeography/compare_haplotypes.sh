84d74064b1ac53b49a2cd09e840abb7668f3e4e8
daisieh_local <daisieh@gmail.com>
2014-09-14 23:25:20 -0400

    rm intermediate files

--- plastome_phylogeography/compare_haplotypes.sh
+++ plastome_phylogeography/compare_haplotypes.sh
@@ -42,7 +42,7 @@ do
 
 			samtools mpileup -B -C50 -I -f $ref -u $sample.sorted.bam > $sample.bcf
 			bcftools view -c $sample.bcf > $sample.vcf
-			rm $sample.bcf $sample.small.bam
+			rm $sample.bcf $sample.sorted.bam $sample.small.bam
 		fi
 		cd ..
 	done < $samplefile

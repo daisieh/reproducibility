e6bf33a604e8c0e9f92d6049f980256ddecdd4c7
daisieh_local <daisieh@gmail.com>
2014-09-28 23:30:12 -0700

    fixed

--- plastome_phylogeography/process_sample_data.sh
+++ plastome_phylogeography/process_sample_data.sh
@@ -4,8 +4,8 @@
 
 #### balsam_sample_data.txt is a file with the list of samples:
 ####1file_id	2sample	3DNA_code	4ADM_#	5species	6POP_CODE	7POPNAME	8POP#	9GROUP	10LONG	11LAT	12server	13path	14plastome
-
-gawk '$0 !~ /^#/' $1 | gawk -F "\t" '{print $11"\t"$1"\t"$12}' |
+####output is server, sample, path
+gawk '$0 !~ /^#/' $1 | gawk -F "\t" '{print $12"\t"$1"\t"$13}' |
 {
 while read line
 do

50e602438cc5fda720829cc388918d1bc9e0b1e8
daisieh_local <daisieh@gmail.com>
2014-09-28 23:27:09 -0700

    print to stdout

--- plastome_phylogeography/process_sample_data.sh
+++ plastome_phylogeography/process_sample_data.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/bash
 
 #### check to remove any files to paths that don't exist on this machine.
-samplefile="samplefile.txt"
-
-rm $samplefile
 
 #### balsam_sample_data.txt is a file with the list of samples:
 ####1file_id	2sample	3DNA_code	4ADM_#	5species	6POP_CODE	7POPNAME	8POP#	9GROUP	10LONG	11LAT	12server	13path	14plastome
@@ -16,7 +13,7 @@ do
 	if [ -f ${arr[2]} ];
 	then
 	echo $line
-	echo $line >> $samplefile;
+# 	echo $line >> $samplefile;
 	fi
 done;
 };

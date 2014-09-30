af6bcc9b21a0b79dcf8ce76cd06316becfffb21f
daisieh_local <daisieh@gmail.com>
2014-09-30 16:30:59 -0700

    sort

--- plastome_phylogeography/subsample_data.sh
+++ plastome_phylogeography/subsample_data.sh
@@ -5,7 +5,7 @@ samplefile="samplefile.txt"
 
 rm $samplefile
 
-#### balsam_sample_data.txt is a file with the list of samples:
+#### $1 is a file with the list of samples:
 ####1file_id	2sample	3DNA_code	4ADM_#	5species	6POP_CODE	7POPNAME	8POP#	9GROUP	10LONG	11LAT	12server	13path	14plastome
 
 gawk '$0 !~ /^#/' $1 | gawk -F " " '{print $13"\t"$1"\t"$14}' |
@@ -26,5 +26,7 @@ do
 	arr=($line);
 	sample=${arr[1]};
 	path=${arr[2]};
-	samtools view -s 85.143 $path > $sample.bam
+# 	samtools view -s 85.143 $path > $sample.bam
+	samtools sort $sample.bam $sample.sorted
+	rm $sample.bam
 done < $samplefile

bbf0987cc31da8034b2a41eb47f89f56a94c3495
daisieh_local <daisieh@gmail.com>
2014-10-23 23:48:34 -0700

    just do draft assembly

--- plastome_phylogeography/denovo_plastomes.sh
+++ plastome_phylogeography/denovo_plastomes.sh
@@ -36,10 +36,10 @@ do
 	$REPOS/phylogenomics/converting/bam_to_fasta.sh $sample.small.bam $sample
 #
 	#### aTRAM libs
-	echo "  making aTRAM db"
-	perl $REPOS/aTRAM/format_sra.pl -in $sample.fasta -out $CWD/aTRAMdbs/$sample -num 10
+# 	echo "  making aTRAM db"
+# 	perl $REPOS/aTRAM/format_sra.pl -in $sample.fasta -out $CWD/aTRAMdbs/$sample -num 10
 #
-	#### if use CLC genomics workbench 7.0.3 to do de novo assembly of the reads
+	#### if using CLC genomics workbench 7.0.3 to do de novo assembly of the reads
 	#### rename the outputted contigs to more sensible names.
 	# sed s/\.small\.bam_.no_read_group._.paired.// < $sample.contigs.fa | sed s/Average\ coverage:.*$// > $sample.contigs.fasta
 #
@@ -64,55 +64,55 @@ echo "Finished initial assembly"
 
 while read sample
 do
-	echo "filling in $sample..."
-#
 	cd $sample
-	echo -e ">$sample\n" > $sample.plastome.final.fasta
-	gawk '$0 !~ />/ { print $0; }' $sample.plastome.draft.fasta >> $sample.plastome.final.fasta
+# 	echo "filling in $sample..."
+# #
+# 	echo -e ">$sample\n" > $sample.plastome.final.fasta
+# 	gawk '$0 !~ />/ { print $0; }' $sample.plastome.draft.fasta >> $sample.plastome.final.fasta
+# #
+# 	#### find sections of ambiguity in the draft plastome:
+# 	grep -o -E ".{200}[Nn]+.{200}" $sample.plastome.final.fasta > $sample.to_atram.txt
+# #
+# 	rm $sample.targets.txt
+# 	count=1
+# 	while read seq
+# 	do
+# 		echo ">$sample.$count" > $sample.$count.fasta
+# 		echo "$seq" >> $sample.$count.fasta
+# 		echo -e "$sample.$count\t$sample.$count.fasta" >> $sample.targets.txt
+# 		count=$(($count+1))
+# 	done < $sample.to_atram.txt
+# 	echo -e "$sample\t$CWD/aTRAMdbs/$sample.atram" > $sample.samples.txt
+# #
+# 	#### aTRAM those ambiguous sections
+# 	echo "  aTRAM ambiguous sections"
+# 	perl $REPOS/aTRAM/Pipelines/BasicPipeline.pl -samples $sample.samples.txt -target $sample.targets.txt -frac 0.3 -iter 10 -out $sample.atram
 #
-	#### find sections of ambiguity in the draft plastome:
-	grep -o -E ".{200}[Nn]+.{200}" $sample.plastome.final.fasta > $sample.to_atram.txt
+# 	#### Now, take the best seq from each one and align it to the draft:
+# 	head -n 1 $sample.plastome.final.fasta > $sample.plastome.0.fasta
+# 	tail -n +2 $sample.plastome.final.fasta | sed s/[Nn\n]/-/g >> $sample.plastome.0.fasta
+# 	for ((i=1;i<10;i++))
+# 	do
+# 		#### if there were atram results, align and consensus:
+# 		if [ -f $sample.atram/$sample/$sample.$i.best.fasta ]
+# 		then
+# 			echo "aligning atram results $i"
+# 			j=$(($i-1))
+# 			echo ">$sample.plastome.$j" > $sample.plastome.$i.fasta
+# 			tail -n +2 $sample.plastome.$j.fasta | sed s/[Nn]/-/g >> $sample.plastome.$i.fasta
+# 			head -n 1 $sample.atram/$sample/$sample.$i.best.fasta >> $sample.plastome.$i.fasta
+# 			head -n 2 $sample.atram/$sample/$sample.$i.best.fasta | tail -n 1 | sed s/[Nn]/-/g >> $sample.plastome.$i.fasta
 #
-	rm $sample.targets.txt
-	count=1
-	while read seq
-	do
-		echo ">$sample.$count" > $sample.$count.fasta
-		echo "$seq" >> $sample.$count.fasta
-		echo -e "$sample.$count\t$sample.$count.fasta" >> $sample.targets.txt
-		count=$(($count+1))
-	done < $sample.to_atram.txt
-	echo -e "$sample\t$CWD/aTRAMdbs/$sample.atram" > $sample.samples.txt
+# 			#### align with large gap-opening penalty: gaps already exist, we don't need to add more.
+# 			mafft --retree 2 --maxiterate 0 --op 10 $sample.plastome.$i.fasta > $sample.plastome.$i.aln.fasta 2>/dev/null
+# 			perl $REPOS/phylogenomics/filtering/consensus.pl $sample.plastome.$i.aln.fasta > $sample.plastome.$i.fasta
+# 			#### final sequence is the last one:
+# 			#### remove gaps in sequence:
+# 			head -n 1 $sample.plastome.$i.fasta > $sample.plastome.final.fasta
+# 			tail -n +2 $sample.plastome.$i.fasta | sed s/-//g >> $sample.plastome.final.fasta
+# 		fi
+# 	done
 #
-	#### aTRAM those ambiguous sections
-	echo "  aTRAM ambiguous sections"
-	perl $REPOS/aTRAM/Pipelines/BasicPipeline.pl -samples $sample.samples.txt -target $sample.targets.txt -frac 0.3 -iter 10 -out $sample.atram
-
-	#### Now, take the best seq from each one and align it to the draft:
-	head -n 1 $sample.plastome.final.fasta > $sample.plastome.0.fasta
-	tail -n +2 $sample.plastome.final.fasta | sed s/[Nn\n]/-/g >> $sample.plastome.0.fasta
-	for ((i=1;i<10;i++))
-	do
-		#### if there were atram results, align and consensus:
-		if [ -f $sample.atram/$sample/$sample.$i.best.fasta ]
-		then
-			echo "aligning atram results $i"
-			j=$(($i-1))
-			echo ">$sample.plastome.$j" > $sample.plastome.$i.fasta
-			tail -n +2 $sample.plastome.$j.fasta | sed s/[Nn]/-/g >> $sample.plastome.$i.fasta
-			head -n 1 $sample.atram/$sample/$sample.$i.best.fasta >> $sample.plastome.$i.fasta
-			head -n 2 $sample.atram/$sample/$sample.$i.best.fasta | tail -n 1 | sed s/[Nn]/-/g >> $sample.plastome.$i.fasta
-
-			#### align with large gap-opening penalty: gaps already exist, we don't need to add more.
-			mafft --retree 2 --maxiterate 0 --op 10 $sample.plastome.$i.fasta > $sample.plastome.$i.aln.fasta 2>/dev/null
-			perl $REPOS/phylogenomics/filtering/consensus.pl $sample.plastome.$i.aln.fasta > $sample.plastome.$i.fasta
-			#### final sequence is the last one:
-			#### remove gaps in sequence:
-			head -n 1 $sample.plastome.$i.fasta > $sample.plastome.final.fasta
-			tail -n +2 $sample.plastome.$i.fasta | sed s/-//g >> $sample.plastome.final.fasta
-		fi
-	done
-
 	#### clean draft plastome
 	perl $REPOS/phylogenomics/plastome/clean_cp.pl -ref $reffile -contig $sample.plastome.final.fasta -out $sample.plastome
 	cd $CWD

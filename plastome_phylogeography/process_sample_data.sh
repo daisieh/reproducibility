b24e1a389866d7c37baf01ecf1aa8598ee5772b0
daisieh_local <daisieh@gmail.com>
2014-10-28 00:15:14 -0700

    rewrite to use new format

--- plastome_phylogeography/process_sample_data.sh
+++ plastome_phylogeography/process_sample_data.sh
@@ -2,10 +2,16 @@
 
 #### check to remove any files to paths that don't exist on this machine.
 
-#### balsam_sample_data.txt is a file with the list of samples:
-####1file_id	2sample	3DNA_code	4ADM_#	5species	6POP_CODE	7POPNAME	8POP#	9GROUP	10LONG	11LAT	12server	13path	14plastome
-####output is server, sample, path
-gawk '$0 !~ /^#/' $1 | gawk -F "\t" '{print $12"\t"$1"\t"$13}' |
+#### all_sample_data.txt is a file with the list of samples:
+####1file_id	2sample	3DNA_code	4species	5pop_code	6pop_name	7LONG	8LAT	9server	10path
+
+#### remove commented lines:
+gawk '$0 !~ /^#/' $1 |
+
+#### then print server, fileid, path
+gawk -F "\t" '{print $9"\t"$1"\t"$10}' |
+
+#### then print only lines that have paths that exist
 {
 while read line
 do
@@ -13,7 +19,6 @@ do
 	if [ -f ${arr[2]} ];
 	then
 	echo $line
-# 	echo $line >> $samplefile;
 	fi
 done;
 };

ae63ad02d207dfdd5c8b7c3cff4518869d8ee950
daisieh_local <daisieh@gmail.com>
2014-12-16 02:43:56 -0800

    summary stats for haplotypes by populations

--- plastome_phylogeography/parse_haplotypes.pl
+++ plastome_phylogeography/parse_haplotypes.pl
@@ -2,43 +2,32 @@
 
 use strict;
 use FindBin;
-use lib "$FindBin::Bin/..";
-use Subfunctions qw(split_seq reverse_complement meld_matrices);
 use Data::Dumper;
-use XML::Writer;
-use XML::Smart;
-  use IO::File;
 
 my $data_file = shift;
 my $haplotypes = shift;
-# my $kml_file = shift;
-
-# my $parser = XML::LibXML->new();
-# my $doc = XML::LibXML->load_xml( location => $kml_file );
-# print $doc->toString();
 my $populations = {};
 my $samples_lookup = {};
 open FH, "<", $data_file;
-#
+
 foreach my $line (<FH>) {
 	if ($line =~ /^#/) {
 		#comment line, skip.
 		next;
 	}
-#
-	#1file_id	2sample	3DNA_code	4species	5pop_code	6pop_name	7LONG	8LAT	9server	10path	11sorted
+# 	1file_id	2sample	3DNA_code	4species	5pop	6pop_name	7LONG	8LAT	9server	10path	11sorted
+
 	my ($file_id, $sample, $dna_code, $species, $pop_code, $pop_name, $long, $lat, undef) = split (/\t/, $line, 9);
-#
+
 	if ($species =~ /PBAL|PTRI/) {
-		$populations->{$species}->{$pop_code}->{$sample}->{"dna_code"} = $dna_code;
-		$populations->{$species}->{$pop_code}->{$sample}->{"lat"} = $lat;
-		$populations->{$species}->{$pop_code}->{$sample}->{"long"} = $long;
-		$samples_lookup->{$sample} = $populations->{$species}->{$pop_code}->{$sample};
+		$populations->{$species}->{$pop_code}->{$file_id}->{"lat"} = $lat;
+		$populations->{$species}->{$pop_code}->{$file_id}->{"long"} = $long;
+		$samples_lookup->{$file_id} = $populations->{$species}->{$pop_code}->{$file_id};
 	}
 }
-#
+
 close FH;
-#
+
 foreach my $species (keys %$populations) {
 	foreach my $pop (keys %{$populations->{$species}}) {
 		my $lat_sum = 0;
@@ -51,9 +40,9 @@ foreach my $species (keys %$populations) {
 		}
 		$populations->{$species}->{$pop}->{"lat"} = $lat_sum / $count;
 		$populations->{$species}->{$pop}->{"long"} = $long_sum / $count;
+		$populations->{$species}->{$pop}->{"total"} = 0;
 	}
 }
-
 open FH, "<", $haplotypes;
 foreach my $line (<FH>) {
 	my ($sample, $hap) = split (/\t/,$line);
@@ -63,85 +52,50 @@ foreach my $line (<FH>) {
 	}
 }
 close FH;
+
 foreach my $species (keys %$populations) {
 	foreach my $pop (keys %{$populations->{$species}}) {
 		if (!exists $populations->{$species}->{$pop}->{"haplotypes"}) {
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"A"} = 0;
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"A1"} = 0;
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"B"} = 0;
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"C"} = 0;
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"D"} = 0;
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"T"} = 0;
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"U"} = 0;
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"Aige"} = 0;
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"Het"} = 0;
-			$populations->{$species}->{$pop}->{"haplotypes"}->{"Other"} = 0;
+			$populations->{$species}->{$pop}->{"haplotypes"}->{"T1"} = 0;
+			$populations->{$species}->{$pop}->{"haplotypes"}->{"T2"} = 0;
+			$populations->{$species}->{$pop}->{"haplotypes"}->{"T3"} = 0;
+			$populations->{$species}->{$pop}->{"haplotypes"}->{"PG"} = 0;
+			$populations->{$species}->{$pop}->{"haplotypes"}->{"B1"} = 0;
+			$populations->{$species}->{$pop}->{"haplotypes"}->{"B2"} = 0;
+			$populations->{$species}->{$pop}->{"haplotypes"}->{"B3"} = 0;
 		}
 		foreach my $sample (keys %{$populations->{$species}->{$pop}}) {
-			if ($sample !~ /lat|long/) {
+			if ($sample !~ /lat|long|total/) {
 				my $hap = $populations->{$species}->{$pop}->{$sample}->{"haplotype"};
 				$populations->{$species}->{$pop}->{"haplotypes"}->{$hap} += 1;
+				if ($hap ne "") {
+					$populations->{$species}->{$pop}->{"total"} += 1;
+				}
 			}
 		}
 	}
 }
-
-print "pop\tspecies\tloc\tA\tA1\tB\tC\tD\tT\tU\n";
+# print Dumper ($populations);
+my @haps = ("T1","T2","T3","PG","B1","B2","B3");
+print "popu\tspecies\tlong\tlat\t" . join("\t", @haps) . "\ttotal\n";
 foreach my $species (keys %$populations) {
 	foreach my $pop (keys %{$populations->{$species}}) {
-		print "$pop\t$species\t";
-		print "-$populations->{$species}->{$pop}->{long}, $populations->{$species}->{$pop}->{lat}\t";
-		print $populations->{$species}->{$pop}->{"haplotypes"}->{"A"} . "\t";
-		print $populations->{$species}->{$pop}->{"haplotypes"}->{"A1"} . "\t";
-		print $populations->{$species}->{$pop}->{"haplotypes"}->{"B"} . "\t";
-		print $populations->{$species}->{$pop}->{"haplotypes"}->{"C"} . "\t";
-		print $populations->{$species}->{$pop}->{"haplotypes"}->{"D"} . "\t";
-		print $populations->{$species}->{$pop}->{"haplotypes"}->{"T"} . "\t";
-		print $populations->{$species}->{$pop}->{"haplotypes"}->{"U"} ;
-		print "\n";
+		if ($populations->{$species}->{$pop}->{"total"} > 0) {
+			my $total = $populations->{$species}->{$pop}->{"total"};
+			print "$pop\t$species\t";
+			print "-$populations->{$species}->{$pop}->{long}\t$populations->{$species}->{$pop}->{lat}\t";
+			foreach my $h (@haps) {
+				my $t = $populations->{$species}->{$pop}->{"haplotypes"}->{$h} / $total;
+				if ($t =~ /(\d\.\d\d).*/) {
+					$t = $1;
+				}
+				if ($t == 0) {
+					$t = 0.000001;
+				}
+				print "$t\t";
+			}
+
+			print "$total\n";
+		}
 	}
 }
-
-# 	<Document>
-# 		<name>balsamifera</name>
-# 		<Placemark>
-# 			<styleUrl>#icon-503-FFFFFF</styleUrl>
-# 			<name>BOY</name>
-# 			<ExtendedData>
-# 				<Data name='D'>
-# 					<value></value>
-# 				</Data>
-# 			</ExtendedData>
-# 			<Point>
-# 				<coordinates>-112.65,54.55,0.0</coordinates>
-# 			</Point>
-# 		</Placemark>
-
-# my $kml_hash = {};
-# $kml_hash->{"Document"}->{"name"} = "test doc";
-# foreach my $species (keys %$populations) {
-# 	my $popcount = 0;
-# 	$kml_hash->{"Document"}->{"Placemark"} = ();
-# 	foreach my $pop (keys %{$populations->{$species}}) {
-# 		my $pop_data = {};
-# 		$pop_data->{"name"} = $pop;
-#
-# 		$pop_data->{"Point"}->{"coordinates"} = "-$populations->{$species}->{$pop}->{long},$populations->{$species}->{$pop}->{lat},0.0";
-# 		push @{$kml_hash->{"Document"}->{"Placemark"}}, $pop_data;
-# 	}
-# }
-# print Dumper($kml_hash);
-#
-# my $xml = XML::Smart->new(q`<?xml version='1.0' encoding='UTF-8'?>
-# <kml xmlns='http://www.opengis.net/kml/2.2'>
-# </kml>`);
-# my $count = 0;
-# # foreach my $species (keys %$populations) {
-# # 	$xml->{"kml"}->{$species}[$count++] = $populations->{$species};
-# # 	foreach my $pop (keys %{$populations->{$species}}) {
-# # 		$xml->{"kml"}->{"pop"} = $pop;
-# # 	}
-# # }
-# $xml->{"kml"} = $kml_hash;
-# $xml->save('output.kml') ;
-# # print $xml->data();

f6cce63d4a21938e68788bc8356cf11aedc41068
daisieh_local <daisieh@gmail.com>
2015-02-22 01:22:43 -0800

    test files

--- plastome_assembly/test/test.sh
+++ plastome_assembly/test/test.sh
@@ -24,28 +24,21 @@ fi
 
 perl $REPOS/phylogenomics/plastome/plann.pl -ref $REPOS/reproducibility/plastome_assembly/balsamifera_cp.gb -fasta $REPOS/reproducibility/plastome_assembly/test/jatropha_cp.fasta -out $REPOS/reproducibility/plastome_assembly/test/jatropha_test_out -organism "Jatropha curcas" -sample "jatropha"
 
-#
-# diff $REPOS/reproducibility/plastome_assembly/test/jatropha_test_out.tbl $REPOS/reproducibility/plastome_assembly/test/jatropha_test.tbl
-#
-# if [ $? -eq 0 ]
-# then
-#   echo "Plann self-test is fine."
-# else
-#   echo "Self-test file has diffs: " >&2
-# fi
-#
+if [ $? -eq 0 ]
+then
+  echo "Jatropha test is fine."
+else
+  echo "Jatropha test has diffs: " >&2
+fi
 
 perl $REPOS/phylogenomics/plastome/plann.pl -ref $REPOS/reproducibility/plastome_assembly/balsamifera_cp.gb -fasta $REPOS/reproducibility/plastome_assembly/test/salix_cp.fasta -out $REPOS/reproducibility/plastome_assembly/test/salix_test_out -organism "Salix interior" -sample "salix"
 
-#
-# diff $REPOS/reproducibility/plastome_assembly/test/salix_test_out.tbl $REPOS/reproducibility/plastome_assembly/test/salix_test.tbl
-#
-# if [ $? -eq 0 ]
-# then
-#   echo "Plann self-test is fine."
-# else
-#   echo "Self-test file has diffs: " >&2
-# fi
-#
+if [ $? -eq 0 ]
+then
+  echo "Salix test is fine."
+else
+  echo "Salix test has diffs: " >&2
+fi
 
-tbl2asn -p $REPOS/reproducibility/plastome_assembly/test/ -t $REPOS/reproducibility/plastome_assembly/test/plann.sbt
+echo "Running tbl2asn"
+tbl2asn -Vbv -p $REPOS/reproducibility/plastome_assembly/test/ -t $REPOS/reproducibility/plastome_assembly/test/plann.sbt
